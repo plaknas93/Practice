ffaff261852e9207dc312ebe71f0091ef4cb7105
plaknas93 <mail2plaknas@gmail.com>
2021-01-12 01:52:17 +0530

    new

--- central_limt.R
+++ central_limt.R
@@ -1,19 +1,24 @@
 
 #CENTRAL LIMIT THEOREM IN ACTION
-data=3000:3400
-data
-class(data)
 
-p_mean=mean(data)
+data=read.csv(file='D:/RWorks/Practice/central_limit_test_file.csv')
+head(data)
+
+data$score
+
+library(ggplot2)
+ggplot(data,aes(x=score))+geom_histogram(fill='cornflower blue',col='red')+theme_bw()+xlab('Scores')+ylab('Frequency')+ggtitle('Class Score Distribution')+geom_vline(xintercept = mean(data$Score_1), color = "red", linetype = "longdash")
+
+p_mean=mean(data$score)
 p_mean
-p_std=sd(data)
+p_std=sd(data$score)
 p_std
 s=0
 m=0
 sam_var=0
 for(i in 1:1000)
 {
-  ss=sample(data,30)
+  ss=sample(data$score,30)
   sam_var[i]=sd(ss)
   s[i]=list(ss)
 }
@@ -23,9 +28,8 @@ s[1]
 sam1= as.numeric(unlist(s[1]))
 sam1
 mean(sam1)
-sam_var
 
-names(s)=1:10000
+names(s)=1:1000
 s1500=data.frame(s)
 s1500
 head(s1500)
@@ -60,3 +64,4 @@ data=data.frame(n,sem)
 data
 ggplot(data=data,aes(x=n,y=sem))+geom_line()+ggtitle("Standard Error with n")+xlab("n") 
 #scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0))
+
---
+++ wc.R
@@ -0,0 +1,48 @@
+# N-Grams in R
+library(tm)
+library(SnowballC)
+library(wordcloud)
+library(RColorBrewer)
+library(ggplot2)
+library(wordcloud2)
+library(rJava)
+library(RWeka)
+
+mydata <-readLines(file.choose())
+mycorpus <- Corpus(VectorSource(mydata))
+mycorpus
+
+# Text Cleaning
+
+# Convert the text to lower case
+mycorpus <- tm_map(mycorpus, content_transformer(tolower))
+# Remove numbers
+mycorpus <- tm_map(mycorpus, removeNumbers)
+# Remove english common stopwords
+mycorpus <- tm_map(mycorpus, removeWords, stopwords("english"))
+# Remove punctuations
+mycorpus <- tm_map(mycorpus, removePunctuation)
+# Eliminate extra white spaces
+mycorpus <- tm_map(mycorpus, stripWhitespace)
+
+as.character(mycorpus[[1]])
+
+# Bigrams 
+minfreq_bigram<-2
+
+token_delim <- " \\t\\r\\n.!?,;\"()"
+bitoken <- NGramTokenizer(mycorpus, Weka_control(min=2,max=2, delimiters = token_delim))
+two_word <- data.frame(table(bitoken))
+sort_two <- two_word[order(two_word$Freq,decreasing=TRUE),]
+wordcloud(sort_two$bitoken,sort_two$Freq,random.order=FALSE,scale = c(2,0.35),min.freq = minfreq_bigram,colors = brewer.pal(8,"Dark2"),max.words=150)
+
+# Trigrams 
+
+minfreq_trigram <- 5
+
+token_delim <- " \\t\\r\\n.!?,;\"()"
+tritoken <- NGramTokenizer(mycorpus, Weka_control(min=3,max=3, delimiters = token_delim))
+three_word <- data.frame(table(tritoken))
+sort_three <- three_word[order(three_word$Freq,decreasing=TRUE),]
+wordcloud(sort_three$tritoken, sort_three$Freq, random.order=FALSE,min.freq = minfreq_trigram,scale = c(1.2,0.35),colors = brewer.pal(8,"Dark2"),max.words=150)
+
